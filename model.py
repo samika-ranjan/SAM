04b8f13139aa0f5edd8ebe590fe529edc8089674
samika-ranjan <106417466+samika-ranjan@users.noreply.github.com>
2024-09-19 02:07:44 -0400

    Create model.py
    Initialized SAM model and set up mask generator and predictor

---
+++ model.py
@@ -0,0 +1,11 @@
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+device = "cpu"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+mask_generator = SamAutomaticMaskGenerator(sam)
+predictor = SamPredictor(sam)
