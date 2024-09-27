dcdc1dc8d493d956dd536019aff9e9147fb86338
samika-ranjan <106417466+samika-ranjan@users.noreply.github.com>
2024-09-27 16:31:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,5 @@ def setup_environment():
     print("PyTorch version:", torch.__version__)
     print("Torchvision version:", torch.__version__)
     print("CUDA is available:", torch.cuda.is_available())
-    
-import sys
-!{sys.executable} -m pip install opencv-python matplotlib
-!{sys.executable} -m pip install 'git+https://github.com/facebookresearch/segment-anything.git'
-!wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+
 
